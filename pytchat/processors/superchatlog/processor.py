582c0e0488e47101d15ed921d0e797f2ba7ebaca
KaitoCross <christian_hund@hotmail.de>
2021-09-22 23:00:33 +0200

    added membership anniversary message into superchat counter: currency "MEM" describes its a member anniversary message, value describes the length of the membership

--- pytchat/processors/superchatlog/processor.py
+++ pytchat/processors/superchatlog/processor.py
@@ -5,6 +5,9 @@ from .custom_encoder import CustomEncoder
 from ..default.renderer.paidmessage import LiveChatPaidMessageRenderer
 from ..default.renderer.paidsticker import LiveChatPaidStickerRenderer
 from ..default.renderer.legacypaid import LiveChatLegacyPaidMessageRenderer
+from ..default.renderer.donation import LiveChatDonationAnnouncementRenderer
+from ..default.renderer.placeholder import LiveChatPlaceholderItemRenderer
+from ..default.renderer.membership import LiveChatMembershipItemRenderer
 from ..default.processor import Chat
 from ..default.processor import Chatdata
 from ..default.processor import DefaultProcessor
@@ -19,7 +22,34 @@ class SuperChatLogProcessor(DefaultProcessor):
         self.first = True
         self.abs_diff = 0
         self.renderers = {
+            "liveChatTextMessageRenderer": None,
             "liveChatPaidMessageRenderer": LiveChatPaidMessageRenderer(),
             "liveChatPaidStickerRenderer": LiveChatPaidStickerRenderer(),
             "liveChatLegacyPaidMessageRenderer": LiveChatLegacyPaidMessageRenderer(),
+            "liveChatMembershipItemRenderer": LiveChatMembershipItemRenderer(),
+            "liveChatDonationAnnouncementRenderer": None,
+            "liveChatPlaceholderItemRenderer": LiveChatPlaceholderItemRenderer()
         }
+        
+    def _parse(self, item):
+        try:
+            key = list(item.keys())[0]
+            renderer = self.renderers.get(key)
+            if key not in self.renderers.keys():
+                print("New key detected:",key,item)
+            if key == "liveChatDonationAnnouncementRenderer":
+                print("DonationKey",item)
+            if renderer is None:
+                return None
+            renderer.setitem(item.get(key), Chat())
+            renderer.settype()
+            renderer.get_snippet()
+            renderer.get_authordetails()
+            rendered_chatobj = renderer.get_chatobj()
+            renderer.clear()
+        except (KeyError, TypeError) as e:
+            logger.error(f"{str(type(e))}-{str(e)} item:{str(item)}")
+            return None
+        
+        return rendered_chatobj
+
