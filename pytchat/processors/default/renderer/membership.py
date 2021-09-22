c24eef866866e75f412fa630fcd09350400ac9ad
KaitoCross <christian_hund@hotmail.de>
2021-09-22 22:38:26 +0200

    shanged membership renderer: now also renders membership anniversary text messages

--- pytchat/processors/default/renderer/membership.py
+++ pytchat/processors/default/renderer/membership.py
@@ -3,16 +3,39 @@ from .base import BaseRenderer
 
 class LiveChatMembershipItemRenderer(BaseRenderer):
     def settype(self):
-        self.chat.type = "newSponsor"
+        if "message" in self.item.keys():
+            self.chat.type = "sponsorMessage"
+        else:
+            self.chat.type = "newSponsor"
 
     def get_authordetails(self):
         super().get_authordetails()
         self.chat.author.isChatSponsor = True
 
     def get_message(self, item):
-        try:
-            message = ''.join([mes.get("text", "")
-                           for mes in item["headerSubtext"]["runs"]])
-        except KeyError:
-            return "Welcome New Member!", ["Welcome New Member!"]
-        return message, [message]
+        message = ''
+        message_ex = []
+        if "message" in item.keys():
+            message, message_ex = super().get_message(item)
+        else:
+            try:
+                message = ''.join([mes.get("text", "")
+                               for mes in item["headerSubtext"]["runs"]])
+                message_ex = [message]
+            except KeyError:
+                return "Welcome New Member!", ["Welcome New Member!"]
+        return message, message_ex
+    
+    def get_snippet(self):
+        super().get_snippet()
+        if "message" in self.item.keys() and "headerPrimaryText" in self.item.keys():
+            runs = self.item.get("headerPrimaryText", {}).get("runs", [])
+            if len(runs) == 3:
+                self.chat.amountString = runs[1]["text"]
+                self.chat.amountValue = float(self.chat.amountString)
+                time = runs[2]["text"].strip()
+                self.chat.currency = "MEMB"
+                if time in ["months","month"]:
+                    self.chat.currency = "MON"
+                if time in ["days","day"]:
+                    self.chat.currency = "DAYS"
