f0fb0189b61bbd207381dc09aed6f1cf9eda3c1b
KaitoCross <christian_hund@hotmail.de>
2022-02-14 21:06:10 +0100

    fix

--- pytchat/util/__init__.py
+++ pytchat/util/__init__.py
@@ -151,6 +151,6 @@ async def get_channelid_async_2nd(client, video_id):
     return ret
 
 async def async_is_member_stream(client, video_id):
-    resp = client.get("https://m.youtube.com/embed/{}".format(quote(video_id)), headers=config.m_headers)  
+    resp = await client.get("https://m.youtube.com/embed/{}".format(quote(video_id)), headers=config.m_headers)  
     match = "BADGE_STYLE_TYPE_MEMBERS_ONLY" in resp.text
     return match
